94abdd3315a3bda019cf0d86f8e0a7d59cacb990
Jay Bazuzi <JAY@BAZUZI.COM>
2013-03-10 19:50:52 -0700

    a static image

---
+++ PacMan/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    public partial class Form1 : Form
+    {
+        Game game = new Game();
+        readonly Graphics graphics;
+        public Form1()
+        {
+            graphics = this.CreateGraphics();
+            InitializeComponent();
+        }
+
+        const int scale = 10;
+        readonly Brush WallBrush = new SolidBrush(Color.DarkBlue);
+        readonly Brush PacManBrush = new SolidBrush(Color.Orange);
+        readonly Brush BigPacManBrush = new SolidBrush(Color.Brown);
+        readonly Brush DotBrush = new SolidBrush(Color.White);
+        readonly Brush[] GhostBrushes = new[] {
+            new SolidBrush(Color.Red),
+            new SolidBrush(Color.Green),
+            new SolidBrush(Color.Pink),
+            new SolidBrush(Color.Blue),
+        };
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var y in game.Tiles.VerticalRange)
+            {
+                foreach (var x in game.Tiles.HorizontalRange)
+                {
+                    Rectangle rectangle = new Rectangle(x * scale, (game.Tiles.Height - y) * scale, scale, scale);
+
+                    if (game.PlayerLocation.x == x && game.PlayerLocation.y == y)
+                    {
+                        graphics.FillRectangle(game.Big > 0 ? this.BigPacManBrush : this.PacManBrush, rectangle);
+
+                    }
+                    else if (game.GhostLocations.Any(gl => gl.x == x && gl.y == y))
+                    {
+                        // TODO: mix colors
+                        graphics.FillRectangle(this.GhostBrushes[0], rectangle);
+                    }
+                    else
+                    {
+                        switch (game.Tiles[x, y])
+                        {
+                            case Game.Tile.Blank:
+                                break;
+
+                            case Game.Tile.Dot:
+                                graphics.FillRectangle(this.DotBrush, rectangle);
+                                break;
+
+                            case Game.Tile.BigDot:
+                                break;
+
+                            case Game.Tile.Wall:
+                                graphics.FillRectangle(this.WallBrush, rectangle);
+                                break;
+
+                            default:
+                                throw new Exception();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- PacMan/Program.cs
+++ PacMan/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
 
 namespace ConsoleApplication1
 {
@@ -10,49 +6,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var game = new Game();
-            Console.Clear();
-            Console.WriteLine(game);
-
-            do
-            {
-                var key = Console.ReadKey();
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.LeftArrow:
-                        game.MoveBy(-1, 0);
-                        break;
-
-                    case ConsoleKey.RightArrow:
-                        game.MoveBy(+1, 0);
-                        break;
-
-                    case ConsoleKey.UpArrow:
-                        game.MoveBy(0, -1);
-                        break;
-
-                    case ConsoleKey.DownArrow:
-                        game.MoveBy(0, +1);
-                        break;
-                }
-
-                Console.Clear();
-                Console.WriteLine(game);
-
-                if (game.Won)
-                {
-                    Console.WriteLine("You win!");
-                    break;
-                }
-
-                if (game.Lost)
-                {
-                    Console.Write("You lose!");
-                    break;
-                }
-
-            } while (true);
+            Application.Run(new Form1());
         }
     }
 }
